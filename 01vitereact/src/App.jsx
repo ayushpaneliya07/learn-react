9d992be6a000a71aed71671423798217f2ec36dd
ayushpaneliya <paneliyaayush@gmail.com>
2024-07-20 13:55:14 +0530

    create a project with vite and normal react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+     <h4>hello word</h4>
+  )
+}
+
+export default App
---
+++ 02learnreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>hello everyone</h1>;
+}
+
+export default App;
